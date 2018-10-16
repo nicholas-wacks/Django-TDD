0bfc3adba027e92c5d65cf4cea256f86d7c66a14
nwacks <nicholas.wacks@gmail.com>
2018-10-16 14:01:28 -0600

    Update functional_tests/tests.py

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
 import time
+import os
 
 MAX_WAIT = 10
 
@@ -10,6 +11,9 @@ class newVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
+        staging_server = os.environ.get('STAGING_SERVER')
+        if staging_server:
+            self.live_server_url = 'http://' + staging_server
 
     def tearDown(self):
         self.browser.quit()
@@ -110,3 +114,28 @@ class newVisitorTest(StaticLiveServerTestCase):
         #Satisfied with her milky decision, she leaves the site, likely without bookmarking her unique URL, 
         #   leaving this list to drift into the ether, untouched until the day the production servers
         #   catch fire and the project is considered lost
+    
+    def test_layout_and_styling(self):
+        #Bob goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        #He notices the input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
+        #He starts a new list and sees the input is nicely
+        #   centered there too
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
