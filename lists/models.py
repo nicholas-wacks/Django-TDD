dcb3c18bb57496388dbc07f3b93b1f6a5c59d850
nwacks <nicholas.wacks@gmail.com>
2018-10-17 16:47:06 -0600

    Prevent item duplicates at model layer

--- lists/models.py
+++ lists/models.py
@@ -9,5 +9,9 @@ class Item(models.Model):
     text = models.TextField(default='')
     list = models.ForeignKey(List, default='')
 
+    def __str__(self):
+        return self.text
+
     class Meta:
+        ordering = ('id',)
         unique_together = ('list', 'text')
